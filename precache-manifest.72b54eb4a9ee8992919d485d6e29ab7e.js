ce153e5ee7a6782527696365c845f03101f2b205
MarjanGit <mansar16@huntersoe.org>
2020-05-18 14:18:02 -0400

    Updates

---
+++ precache-manifest.72b54eb4a9ee8992919d485d6e29ab7e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c3c7430373661d64b84ab838cc37010",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0107c80fd7176d20dd8",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "fc048d618e5c99de303b",
+    "url": "/static/css/main.f910c630.chunk.css"
+  },
+  {
+    "revision": "b0107c80fd7176d20dd8",
+    "url": "/static/js/2.ff3a420a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.ff3a420a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc048d618e5c99de303b",
+    "url": "/static/js/main.24347370.chunk.js"
+  },
+  {
+    "revision": "c882cea1c2b4bbb3c7e9",
+    "url": "/static/js/runtime-main.8dbf54ec.js"
+  }
+]);
